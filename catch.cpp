d9b518f0b48db0a0f7cc43c1076ca78fc3a6ffc4
cmg189 <cmg189@outlook.com>
2022-06-12 16:02:35 -0500

    accessor and mutator for weight variable

--- catch.cpp
+++ catch.cpp
@@ -4,13 +4,15 @@
 Catch:: Catch(){
   log_time = 0;
   log_location = "default";
+  log_weight = 00.00;
 }
 Catch:: ~Catch(){ }
 
 // class initalization
-Catch:: Catch(int catch_time, string catch_location){
+Catch:: Catch(int catch_time, string catch_location, float catch_weight){
   this->log_time = catch_time;
   this->log_location = catch_location;
+  this->log_weight = catch_weight;
 }
 
 // time set and get
@@ -24,5 +26,11 @@ void Catch:: set_time(int catch_time){
 string Catch:: get_location(){ return log_location; }
 void Catch:: set_location(string catch_location){
   this->log_location = catch_location;
+  return;
+}
+
+float Catch:: get_weight(){ return log_weight; }
+void Catch:: set_weight(float catch_weight){
+  this->log_weight = catch_weight;
   return;
 }
