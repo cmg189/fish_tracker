d6254c0a0976e1a49c7d2999be96501642d097c2
cmg189 <cmg189@outlook.com>
2022-06-12 16:01:49 -0500

    added weight member variable

--- catch.h
+++ catch.h
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 class Catch{
   private:
     int log_time;
     string log_location;
+    float log_weight;
 
   public:
     Catch(); ~Catch();
-    Catch(int catch_time, string catch_location);
+    Catch(int catch_time, string catch_location, float catch_weight);
 
 
     int get_time();
@@ -18,4 +20,8 @@ class Catch{
 
     string get_location();
     void set_location(string catch_location);
+
+    float get_weight();
+    void set_weight(float catch_weight);
+
 };
